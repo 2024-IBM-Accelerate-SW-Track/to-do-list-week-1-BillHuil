ef8c605c81740fd7a063b2d722a6fd6e8c6c01fd
BillHuil <92352799+BillHuil@users.noreply.github.com>
2024-06-06 00:23:06 -0400

    About Me changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-  
+import "../pages/About.css"
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Bill Huillca
-          This is about me. </p> 
+        <div className="split left">
+          <div className="centered">
+
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Bill Huillca</div>
+            <div className="brief_description">
+              Hi! My name is Bill and I'm a CS student
+              at the University Of Maryland - College Park.
+              Some o
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
