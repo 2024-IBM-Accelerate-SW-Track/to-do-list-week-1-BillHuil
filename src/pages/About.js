8cad8ccf18e8f4fd206242ecdd9e5b23d398ccca
BillHuil <92352799+BillHuil@users.noreply.github.com>
2024-06-07 00:14:22 -0400

    uploaded photo and edited about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,16 +7,22 @@ export default class About extends Component {
       <div>
         <div className="split left">
           <div className="centered">
-
+            <img className="profile_image"
+            src="/src/assets/<profile_image.jpg>"
+            alt="Profile Pic">
+            </img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
             <div className="name_title">Bill Huillca</div>
             <div className="brief_description">
-              Hi! My name is Bill and I'm a CS student
-              at the University Of Maryland - College Park.
-              Some o
+              Hi! My name is Bill Huillca and I'm a computer science
+              student at the University of Maryland - College Park.
+              I enjoy programming, going to the gym, running, and attending
+              concerts. A fun fact about me is that I found my passion in
+              software development through using automated bots
+              to buy shoes that I wanted as a teenager.
             </div>
           </div>
         </div>
