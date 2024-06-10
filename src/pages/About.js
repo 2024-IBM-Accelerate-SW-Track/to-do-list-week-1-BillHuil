a73c2a314e56165dbb3fda5c839e77572d38770a
BillHuil <92352799+BillHuil@users.noreply.github.com>
2024-06-09 20:27:47 -0400

    Added LinkedIn Button

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import "./About.css";
-import profilePicture from "/Users/bacca/github-classroom/2024-IBM-Accelerate-SW-Track/to-do-list-week-1-BillHuil/src/assets/profile_me.jpg";
+import profilePicture from "../assets/profile_me.jpg";
+import { Link } from 'react-router-dom';
+import { Button } from "@mui/material";
 
 export default class About extends Component {
   render() {
@@ -18,16 +20,27 @@ export default class About extends Component {
           <div className="centered">
             <div className="name_title">Bill Huillca</div>
             <div className="brief_description">
-              Hi! My name is Bill Huillca and I'm a computer science
+              <p>Hi! My name is Bill Huillca and I'm a computer science
               student at the University of Maryland - College Park.
               I enjoy programming, going to the gym, running, and attending
               concerts. A fun fact about me is that I found my passion in
               software development through using automated bots
-              to buy shoes that I wanted as a teenager.
+              to buy shoes that I wanted as a teenager.</p>
             </div>
+            <LinkedInLink />
           </div>
         </div>
       </div>
     )
   }
+}
+
+function LinkedInLink(){
+  return(
+    <Link to="https://www.linkedin.com/in/bill-huillca-6b58021a9/">
+      <Button variant="contained"color="primary">
+        Let's Connect!
+        </Button>
+        </Link>
+  );
 }
